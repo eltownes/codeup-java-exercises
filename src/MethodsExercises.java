b30b7bf4517d02733024323f9fa5c5617824fa33
eltownes <50155313+eltownes@users.noreply.github.com>
2023-12-14 16:59:55 -0500

    method - exer 2 complete

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,20 +1,32 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
+
+
 
     public static void main(String[] args) {
 
-        // basic arithmetic
+
+//        1 basic arithmetic
         int num1 = 15;
         int num2 = 3;
-        System.out.printf("%d + %d = " + add(num1,num2) + "%n", num1, num2);
-        System.out.printf("%d - %d = " + subtract(num1,num2) + "%n", num1, num2);
-        System.out.printf("%d * %d = " + multiply(num1,num2) + "%n", num1, num2);
-        System.out.printf("%d / %d = " + divide(num1,num2) + "%n", num1, num2);
+        System.out.printf("%d + %d = " + add(num1, num2) + "%n", num1, num2);
+        System.out.printf("%d - %d = " + subtract(num1, num2) + "%n", num1, num2);
+        System.out.printf("%d * %d = " + multiply(num1, num2) + "%n", num1, num2);
+        System.out.printf("%d / %d = " + divide(num1, num2) + "%n", num1, num2);
 
+
+//      2
+        int userInput;
+        do {
+            System.out.print("Enter a number between 1 and 10: ");
+            userInput = getInteger(1, 10);
+        } while (userInput == 0);
 
 
     }
 
-    // basic arithmetic
+    // 1 basic arithmetic
     public static int add (int num1, int num2){
         return num1 + num2;
     }
@@ -29,7 +41,21 @@ public class MethodsExercises {
     }
 
 
-    //
+    // 2
+    public static int getInteger(int min, int max){
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int ret = 0;
+        if (num >= min && num <= max){
+            ret = num;
+        }
+        return ret;
+    }
+
+
+
+
 
 
 
