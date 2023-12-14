a0405e4c92a90866834180a79f4a01fbd2cabeea
eltownes <50155313+eltownes@users.noreply.github.com>
2023-12-14 17:56:03 -0500

    method - exer 4 almost complete - done for the day

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,8 +1,6 @@
-import javax.swing.plaf.TableHeaderUI;
 import java.util.Scanner;
 
 public class MethodsExercises {
-
 
 
     public static void main(String[] args) {
@@ -32,57 +30,84 @@ public class MethodsExercises {
         boolean verifyNum = numFact >= 1 && numFact <= 10;
         System.out.println("The number is: " + numFact + " and the verification is..." + verifyNum + ".");
 
-        System.out.print("Do you want to continue [y/N]");
-        if (sc.next().equalsIgnoreCase("y")){
+        System.out.print("Do you want to continue [y/N] ");
+        if (sc.next().equalsIgnoreCase("y")) {
             System.out.println("here");
             long fact = calcFactorial(numFact);
             System.out.println("The factorial of numFact is: " + fact);
         }
 
 
+//      4 dice rolling
+        do {
+            System.out.print("Enter # of sides for the 2 dice: ");
+            int sides = sc.nextInt();
+            System.out.print("Hit [any key] to roll the dice. ");
+            // clear buffer
+            sc.nextLine();
+            String cont = sc.nextLine();
+
+            int[] rolledDice = rollDice(sides);
+
+            System.out.println(rolledDice[0] + " " + rolledDice[1]);
+
+            System.out.print("\nContinue? [y/n] : ");
+        } while (sc.next().equalsIgnoreCase("y"));
 
 
-
-    }
+    } // end main
 
     // 1 basic arithmetic
-    public static int add (int num1, int num2){
+    public static int add(int num1, int num2) {
         return num1 + num2;
     }
-    public static int subtract (int num1, int num2){
+
+    public static int subtract(int num1, int num2) {
         return num1 - num2;
     }
-    public static int multiply (int num1, int num2){
+
+    public static int multiply(int num1, int num2) {
         return num1 * num2;
     }
-    public static int divide (int num1, int num2){
+
+    public static int divide(int num1, int num2) {
         return num1 / num2;
     }
 
 
     // 2 verify
-    public static int getInteger(int min, int max){
+    public static int getInteger(int min, int max) {
         Scanner sc = new Scanner(System.in);
 
         int num = sc.nextInt();
         int ret = 0;
-        if (num >= min && num <= max){
+        if (num >= min && num <= max) {
             ret = num;
         }
         return ret;
     }
 
 
-//    3 factorial
-    public static long calcFactorial(long num){
+    //  3 factorial
+    public static long calcFactorial(long num) {
         // base case
         if (num == 1) {
             return num;
         }
-        num = num * calcFactorial(num-1);
+        num = num * calcFactorial(num - 1);
         return num;
     }
 
+
+    // 4 dice rolling
+    public static int[] rollDice(int sides) {
+
+        int [] rolledArray = new int[sides] ;
+        for (int i = 0; i < sides; i++) {
+            rolledArray[i] = (int)(Math.random() * ((sides - 1) + 1)) + 1;
+        }
+        return rolledArray;
+    }
 
 
 
