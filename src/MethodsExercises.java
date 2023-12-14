ed8e4fff68e50ceb47a365f7749c436fcc0dc04b
eltownes <50155313+eltownes@users.noreply.github.com>
2023-12-14 17:34:10 -0500

    method - exer 3 complete

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,4 @@
+import javax.swing.plaf.TableHeaderUI;
 import java.util.Scanner;
 
 public class MethodsExercises {
@@ -6,8 +7,9 @@ public class MethodsExercises {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
-//        1 basic arithmetic
+//      1 basic arithmetic
         int num1 = 15;
         int num2 = 3;
         System.out.printf("%d + %d = " + add(num1, num2) + "%n", num1, num2);
@@ -16,12 +18,29 @@ public class MethodsExercises {
         System.out.printf("%d / %d = " + divide(num1, num2) + "%n", num1, num2);
 
 
-//      2
+//      2 verify input
         int userInput;
         do {
             System.out.print("Enter a number between 1 and 10: ");
             userInput = getInteger(1, 10);
         } while (userInput == 0);
+
+
+//      3 factorial
+        System.out.print("Factorial, enter a number from 1 and 10: ");
+        int numFact = sc.nextInt();
+        boolean verifyNum = numFact >= 1 && numFact <= 10;
+        System.out.println("The number is: " + numFact + " and the verification is..." + verifyNum + ".");
+
+        System.out.print("Do you want to continue [y/N]");
+        if (sc.next().equalsIgnoreCase("y")){
+            System.out.println("here");
+            long fact = calcFactorial(numFact);
+            System.out.println("The factorial of numFact is: " + fact);
+        }
+
+
+
 
 
     }
@@ -41,7 +60,7 @@ public class MethodsExercises {
     }
 
 
-    // 2
+    // 2 verify
     public static int getInteger(int min, int max){
         Scanner sc = new Scanner(System.in);
 
@@ -54,6 +73,15 @@ public class MethodsExercises {
     }
 
 
+//    3 factorial
+    public static long calcFactorial(long num){
+        // base case
+        if (num == 1) {
+            return num;
+        }
+        num = num * calcFactorial(num-1);
+        return num;
+    }
 
 
 
